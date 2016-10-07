1b7f7dacceaf1ede7bd84be5f3ba206ca1580cf0
luan <luanliang23@gmail.com>
2016-10-08 07:56:19 +0800

    提交10月8号

---
+++ js/loadXml.js
@@ -0,0 +1,35 @@
+// 加载xml文档
+       var loadXML = function (xmlFile) {
+            var xmlDoc;
+            if (window.ActiveXObject) {
+                xmlDoc = new ActiveXObject('Microsoft.XMLDOM');//IE浏览器
+                xmlDoc.async = false;
+                xmlDoc.load(xmlFile);
+            }
+            else if (isFirefox=navigator.userAgent.indexOf("Firefox")>0) { //火狐浏览器
+            //else if (document.implementation && document.implementation.createDocument) {//这里主要是对谷歌浏览器进行处理
+                xmlDoc = document.implementation.createDocument('', '', null);
+                xmlDoc.load(xmlFile);
+            }
+            else{ //谷歌浏览器
+              var xmlhttp = new window.XMLHttpRequest();
+                xmlhttp.open("GET",xmlFile,false);
+                xmlhttp.send(null);
+                if(xmlhttp.readyState == 4){
+                xmlDoc = xmlhttp.responseXML.documentElement;
+                } 
+            }
+            //
+            return xmlDoc;
+        }
+
+        // 首先对xml对象进行判断
+      checkXMLDocObj = function (xmlFile) {
+            var xmlDoc = loadXML(xmlFile);
+            if (xmlDoc == null) {
+                alert('不支持xml文件读取,于是本页面禁止您的操作,推荐使用IE5.0以上可以解决此问题!');
+                window.location.href = '../err.html';
+
+            }
+            return xmlDoc;
+        }
