d1e6363dd8b105ba3994164abd965de2c608d3c6
luanMac <luanliang23@gmail.com>
2016-10-17 09:48:43 +0800

    增加网络登录模块

--- js/HttpCilents.js
+++ js/HttpCilents.js
@@ -35,3 +35,75 @@
         httprequest.send();
       
     }
+    
+    LoginRequestHttpClient=function(RequestData,fun){
+        var  httprequest = new XMLHttpRequest();
+      
+        httprequest.withCredentials = true;
+        console.log("url="+URL.COMMON+RequestData);
+        httprequest.open("GET", URL.COMMON + RequestData,true);//
+        //httprequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        httprequest.ontimeout = function(){
+            console.log(" timeout...");
+            httprequest.abort();
+            fun(0);
+        };
+        httprequest.onreadystatechange=function(){
+            if(httprequest.readyState == 4){
+                if (httprequest.status == 200 ){
+                    var httpResponse = httprequest.responseText;
+                    try{
+                        var getjosn = JSON.parse(httpResponse);
+                        var jsonbool = getjosn["code"];
+                        if(jsonbool!=null){
+                            fun(httpResponse);
+                        }else{
+                            fun(0);
+                        }
+                    }catch(e){
+                        cc.log(e.name + ":" + e.message);
+                    }
+                }else{
+                    fun(0);
+                }
+            }
+        }
+        httprequest.send();
+      
+    }
+    
+    SendEmileRequestHttpClient=function(RequestData,fun){
+        var  httprequest = new XMLHttpRequest();
+      
+        httprequest.withCredentials = true;
+        console.log("url="+URL.COMMON+RequestData);
+        httprequest.open("GET", URL.COMMON + RequestData,true);//
+        //httprequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        httprequest.ontimeout = function(){
+            console.log(" timeout...");
+            httprequest.abort();
+            fun(0);
+        };
+        httprequest.onreadystatechange=function(){
+            if(httprequest.readyState == 4){
+                if (httprequest.status == 200 ){
+                    var httpResponse = httprequest.responseText;
+                    try{
+                        var getjosn = JSON.parse(httpResponse);
+                        var jsonbool = getjosn["code"];
+                        if(jsonbool!=null){
+                            fun(httpResponse);
+                        }else{
+                            fun(0);
+                        }
+                    }catch(e){
+                        cc.log(e.name + ":" + e.message);
+                    }
+                }else{
+                    fun(0);
+                }
+            }
+        }
+        httprequest.send();
+      
+    }
--- js/app.js
+++ js/app.js
@@ -12,21 +12,35 @@
 		loginInfo = loginInfo || {};
 		loginInfo.account = loginInfo.account || '';
 		loginInfo.password = loginInfo.password || '';
-		if (loginInfo.account.length < 5) {
-			return callback('账号最短为 5 个字符');
+		if(loginInfo.account.length != 11) {
+			return callback('请输入正确手机号码');
 		}
-		if (loginInfo.password.length < 6) {
+		if(loginInfo.password.length < 6) {
 			return callback('密码最短为 6 个字符');
 		}
-		var users = JSON.parse(localStorage.getItem('$users') || '[]');
-		var authed = users.some(function(user) {
-			return loginInfo.account == user.account && loginInfo.password == user.password;
-		});
-		if (authed) {
-			return owner.createState(loginInfo.account, callback);
-		} else {
-			return callback('用户名或密码错误');
-		}
+
+		var tokenstr = loginInfo.account + GC.key;
+
+		//var hash = hex_md5("123dafd");
+		var token = md5(tokenstr);
+		
+		var RequestData = "method=login&mobile=" + loginInfo.account + "&password=" + loginInfo.password + "&token=" + token;
+		
+		LoginRequestHttpClient(RequestData,function(data){
+			console.log("data=" + data);
+			if(data != 0) {
+				var json = JSON.parse(data);
+				var code = json['code'];
+				if(code == 1) {
+					return callback();
+				} else {
+					return callback(json['message']);
+				}
+			}
+			
+		})
+
+
 	};
 
 	owner.createState = function(name, callback) {
@@ -46,31 +60,35 @@
 		regInfo.account = regInfo.account || '';
 		regInfo.password = regInfo.password || '';
 		regInfo.pollcode = regInfo.pollcode || '';
-		
-		if (regInfo.account.length < 5) {
+
+		if(regInfo.account.length < 5) {
 			return callback('用户名最短需要 5 个字符');
 		}
-		if (regInfo.password.length < 6) {
+		if(regInfo.password.length < 6) {
 			return callback('密码最短需要 6 个字符');
 		}
-		if (!checkEmail(regInfo.email)) {
+		if(!checkEmail(regInfo.email)) {
 			return callback('邮箱地址不合法');
 		}
-		
-		var tokenstr = regInfo.account + GC.key;
 
+		var tokenstr = regInfo.account + GC.key;
 		//var hash = hex_md5("123dafd");
 		var token = md5(tokenstr);
-
-		
 		//me thod=regist&mobile=15600901539&password=111111&email=2622638480@qq.com&registcode=BY3SBA367644&token=f36b2e07f1d7c7910bb7f7bd665fa260
-		 var RequestData = "method=regist&mobile="+regInfo.account+"&password="+regInfo.password+"&email="+regInfo.email+"&registcode="+regInfo.pollcode+"&token="+token;
-		
-		RegRequestHttpClient(RequestData,function(data){
-			console.log("data="+data);
+		var RequestData = "method=regist&mobile=" + regInfo.account + "&password=" + regInfo.password + "&email=" + regInfo.email + "&registcode=" + regInfo.pollcode + "&token=" + token;
+
+		RegRequestHttpClient(RequestData, function(data) {
+			console.log("data=" + data);
+			if(data != 0) {
+				var json = JSON.parse(data);
+				var code = json['code'];
+				if(code == 1) {
+					return callback();
+				} else {
+					return callback(json['message']);
+				}
+			}
 		})
-		
-		return callback();
 	};
 
 	/**
@@ -94,18 +112,51 @@
 
 	var checkEmail = function(email) {
 		email = email || '';
-		return (email.length > 3 && email.indexOf('@') > -1);
+		return(email.length > 3 && email.indexOf('@') > -1);
 	};
 
 	/**
 	 * 找回密码
 	 **/
-	owner.forgetPassword = function(email, callback) {
+	owner.forgetPassword = function(forgetpasswordInfo, callback) {
 		callback = callback || $.noop;
-		if (!checkEmail(email)) {
+		forgetpasswordInfo = forgetpasswordInfo || {};
+		forgetpasswordInfo.account = forgetpasswordInfo.account || '';
+		forgetpasswordInfo.emailBox = forgetpasswordInfo.emailBox || '';
+		
+		
+		if(forgetpasswordInfo.account.length != 11) {
+			return callback('请输入正确手机号码');
+		}
+		
+		callback = callback || $.noop;
+		if(!checkEmail(forgetpasswordInfo.emailBox)) {
 			return callback('邮箱地址不合法');
 		}
-		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+		
+		var tokenstr = forgetpasswordInfo.account + GC.key;
+
+		//var hash = hex_md5("123dafd");
+		var token = md5(tokenstr);
+		
+		var RequestData = "method=getpassword&mobile=" + forgetpasswordInfo.account + "&email=" + forgetpasswordInfo.emailBox + "&token=" + token;
+		
+		SendEmileRequestHttpClient(RequestData,function(data){
+			console.log("data=" + data);
+			if(data != 0) {
+				var json = JSON.parse(data);
+				var code = json['code'];
+				if(code == 1) {
+					return callback(null, '密码已经发送到您的邮箱，请查收邮件。');
+				} else {
+					return callback(json['message']);
+				}
+			}
+			
+		})
+		
+		
+		
 	};
 
 	/**
@@ -127,10 +178,10 @@
 		 * 获取本地是否安装客户端
 		 **/
 	owner.isInstalled = function(id) {
-		if (id === 'qihoo' && mui.os.plus) {
+		if(id === 'qihoo' && mui.os.plus) {
 			return true;
 		}
-		if (mui.os.android) {
+		if(mui.os.android) {
 			var main = plus.android.runtimeMainActivity();
 			var packageManager = main.getPackageManager();
 			var PackageManager = plus.android.importClass(packageManager)
@@ -141,9 +192,9 @@
 			}
 			try {
 				return packageManager.getPackageInfo(packageName[id], PackageManager.GET_ACTIVITIES);
-			} catch (e) {}
+			} catch(e) {}
 		} else {
-			switch (id) {
+			switch(id) {
 				case "qq":
 					var TencentOAuth = plus.ios.import("TencentOAuth");
 					return TencentOAuth.iphoneQQInstalled();
